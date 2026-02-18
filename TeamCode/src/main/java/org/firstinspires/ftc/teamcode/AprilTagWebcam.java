f0b0b64d9ce0e10ff5964cff796be72d5cf93b30
SClarke <sclarke@gracechristian.net>
2026-02-18 16:53:05 -0500

    Began april tag webcame class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagWebcam.java
@@ -15,7 +15,7 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import java.util.ArrayList;
 import java.util.List;
 
-public class AprilTag {
+public class AprilTagWebcam {
     private AprilTagProcessor aprilTagProcessor;
     private VisionPortal visionPortal;
 
